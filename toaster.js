ad9b051f45b0a4e4394f1677c26ce57ed785aaf2
João Gabriel <jgobi@ufmg.br>
2017-09-16 19:30:21 -0300

    bug fixes and improvements

--- toaster.js
+++ toaster.js
@@ -18,15 +18,6 @@ $.toaster = {
 			 * 		- closeOnClick: boolean
 			 * 		- defaultTimeout: number
 			 */
-			 
-			var willCallFixPositionFunction = false;
-			if (typeof options.initialTop == 'number')
-				willCallFixPositionFunction = true;
-				
-			if (typeof options.appearOnLeft == 'boolean'){
-				$.toaster.appearOnLeft = options.appearOnLeft;
-				willCallFixPositionFunction = true;
-			}
 				
 			if (typeof options.closeOnClick == 'boolean')
 				$.toaster.closeOnClick = options.closeOnClick;
@@ -34,28 +25,39 @@ $.toaster = {
 			if (typeof options.defaultTimeout == 'number')
 				$.toaster.defaultTimeout = options.defaultTimeout;
 			
-			
-			if(willCallFixPositionFunction){
-				$.toaster.fixPosition((typeof options.initialTop == 'number') ? options.initialTop : $.toaster.initialTop);
-			}
+			$.toaster.fixPosition(
+				(typeof options.initialTop == 'number' ? options.initialTop : $.toaster.initialTop),
+				(typeof options.appearOnLeft == 'boolean' ? options.appearOnLeft : $.toaster.appearOnLeft)
+			);
 		},
 		
-		fixPosition: function(newInitialTop){
+		fixPosition: function(newInitialTop, newAppearOnLeft){
 			/**
 			 * This is a PRIVATE function and was not designed to be direct called. Please don't do it.
 			 * Fix the position of toasts when initial top or the side change.
 			 * 
 			 * @param newInitialTop: number
+			 * @param newAppearOnLeft: boolean
 			 */
 			var difference = $.toaster.initialTop - newInitialTop; //evaluate the difference between the new and the old top for moving the existing toasts to the right place.
+			
 			for(i=0;i<$.toaster.activeToasts.length;i++){ //For each toast
-				$( "#toast-"+$.toaster.activeToasts[i] ).css({ //Move it to the rigth side WITHOUT an animation (TODO: put animation here)
-					left: ($.toaster.appearOnLeft ? 0 : ''),
-					right: ($.toaster.appearOnLeft ? '' : 0)
-				}).animate({ //Move it to the rigth top with an animation
+				$( "#toast-"+$.toaster.activeToasts[i] ).animate({ //Move it to the rigth top with an animation
 					top: parseInt($("#toast-"+$.toaster.activeToasts[i] ).css("top"))-difference
 				}, 100);
 			}
+			if(newAppearOnLeft != $.toaster.appearOnLeft){ //if appear on left prop has changed
+				for(i=0;i<$.toaster.activeToasts.length;i++){ //For each toast
+					$( "#toast-"+$.toaster.activeToasts[i] ).slideUp("fast",function(){ //Hide the toasts with animation
+						$(this).css({ //Move it to the rigth side
+							left: ($.toaster.appearOnLeft ? 0 : ''),
+							right: ($.toaster.appearOnLeft ? '' : 0)
+						}).slideDown("fast"); //Show again the toasts with animation
+					});
+				}
+			}
+			
+			$.toaster.appearOnLeft = newAppearOnLeft; //Set the appeatOnLeft to new value
 			$.toaster.initialTop = newInitialTop; //Set the top position to new value
 			$.toaster.currentTop -= difference; //Update the next toast position
 		},
@@ -77,21 +79,21 @@ $.toaster = {
 			$.toaster.activeToasts.push($.toaster.currentId); //add the new toast to the active toasts array
 			if( $(window).width() <= 720 ){ //smartphones - will show the toast in the bottom, and only one once.
 				if((typeof options.closeOnClick == 'boolean' && options.closeOnClick) || (typeof options.closeOnClick == 'undefined' && $.toaster.closeOnClick)) //closeOnClick
-					var toast='<div onclick="$.toaster.hide('+r+')" ';
+					var toast='<div onclick="$.toaster.hide('+r+')" style="cursor: pointer; ';
 				else //close only on X
-					var toast='<div ';
+					var toast='<div style=" ';
 					
-				toast+='style="z-index: 999999; width: 100%; position: fixed; left: 0; bottom: 0px; margin:0px" id="toast-' +r+'" data-alert class="alert-box '+type+'">'+message+' &nbsp;&nbsp;&nbsp;<a href="#" onclick="$.toaster.hide('+r+')" class="close">&times;</a></div>';
+				toast+='z-index: 999999; width: 100%; position: fixed; left: 0; bottom: 0px; margin:0px" id="toast-' +r+'" data-alert class="alert-box '+type+'">'+message+' &nbsp;&nbsp;&nbsp;<a href="#" onclick="$.toaster.hide('+r+')" class="close">&times;</a></div>';
 			}else{ //desktop - will show the toast in the left or rigth side, as defined on appearOnLeft prop.
 				if((typeof options.closeOnClick == 'boolean' && options.closeOnClick) || (typeof options.closeOnClick == 'undefined' && $.toaster.closeOnClick)) //closeOnClick
-					var toast='<div onclick="$.toaster.hide('+r+')" ';
+					var toast='<div title="Clique para fechar." onclick="$.toaster.hide('+r+')" style="cursor: pointer; ';
 				else
-					var toast='<div ';
+					var toast='<div style=" ';
 					
 				if($.toaster.appearOnLeft) //appearOnLeft
-					toast+='style="left: 0; ';
+					toast+='left: 0; ';
 				else //appearOnRigth
-					toast+='style="right: 0; ';
+					toast+='right: 0; ';
 					
 				toast+='z-index: 999999; position: fixed; top: '+$.toaster.currentTop+'px; margin:20px" id="toast-'+r+'" data-alert class="alert-box '+type+'">'+message+' &nbsp;&nbsp;&nbsp;<a href="#" onclick="$.toaster.hide('+r+')" class="close">&times;</a></div>';
 				
