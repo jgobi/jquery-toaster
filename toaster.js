30d2e6b5ca0fca8e0cdcf4eb72d2792b639e1994
João Gabriel <joaogabrieloliveira9@gmail.com>
2018-02-09 11:39:42 -0200

    grammar

--- toaster.js
+++ toaster.js
@@ -4,7 +4,7 @@ $.toaster = {
 	currentId: 0, //internal id for toasts
 	currentTop: 40, //the 'top' css rule of the next toast (the position of the top vertex of the next toast)
 	initialTop: 40, //the position of the top vertex of the first toast
-	appearOnLeft: false, //if the toasts will appear on the left or in the rigth (default) side of the screen
+	appearOnLeft: false, //if the toasts will appear on the left or in the right (default) side of the screen
 	closeOnClick: true, //if the toasts will close if user clicks on any part of it
 	defaultTimeout: 5000, //default time for toast automatically vanish
 	
@@ -43,14 +43,14 @@ $.toaster = {
 		var difference = $.toaster.initialTop - newInitialTop; //evaluate the difference between the new and the old top for moving the existing toasts to the right place.
 		
 		for(i=0;i<$.toaster.activeToasts.length;i++){ //For each toast
-			$( "#toast-"+$.toaster.activeToasts[i] ).animate({ //Move it to the rigth top with an animation
+			$( "#toast-"+$.toaster.activeToasts[i] ).animate({ //Move it to the right top with an animation
 				top: parseInt($("#toast-"+$.toaster.activeToasts[i] ).css("top"))-difference
 			}, 100);
 		}
 		if(newAppearOnLeft != $.toaster.appearOnLeft){ //if appear on left prop has changed
 			for(i=0;i<$.toaster.activeToasts.length;i++){ //For each toast
 				$( "#toast-"+$.toaster.activeToasts[i] ).slideUp("fast",function(){ //Hide the toasts with animation
-					$(this).css({ //Move it to the rigth side
+					$(this).css({ //Move it to the right side
 						left: ($.toaster.appearOnLeft ? 0 : ''),
 						right: ($.toaster.appearOnLeft ? '' : 0)
 					}).slideDown("fast"); //Show again the toasts with animation
@@ -58,7 +58,7 @@ $.toaster = {
 			}
 		}
 		
-		$.toaster.appearOnLeft = newAppearOnLeft; //Set the appeatOnLeft to new value
+		$.toaster.appearOnLeft = newAppearOnLeft; //Set the appearOnLeft to new value
 		$.toaster.initialTop = newInitialTop; //Set the top position to new value
 		$.toaster.currentTop -= difference; //Update the next toast position
 	},
@@ -84,7 +84,7 @@ $.toaster = {
 				var toast='<div style=" ';
 				
 			toast+='z-index: 999999; width: 100%; position: fixed; left: 0; bottom: 0px; margin:0px" id="toast-' +$.toaster.currentId+'" data-alert class="alert-box '+type+'">'+message+' &nbsp;&nbsp;&nbsp;<a href="#" onclick="$.toaster.hide('+$.toaster.currentId+')" class="close">&times;</a></div>';
-		}else{ //desktop - will show the toast in the left or rigth side, as defined on appearOnLeft prop.
+		}else{ //desktop - will show the toast in the left or right side, as defined on appearOnLeft prop.
 			if((typeof options.closeOnClick == 'boolean' && options.closeOnClick) || (typeof options.closeOnClick == 'undefined' && $.toaster.closeOnClick)) //closeOnClick
 				var toast='<div title="Clique para fechar." onclick="$.toaster.hide('+$.toaster.currentId+')" style="cursor: pointer; ';
 			else
@@ -92,14 +92,14 @@ $.toaster = {
 				
 			if($.toaster.appearOnLeft) //appearOnLeft
 				toast+='left: 0; ';
-			else //appearOnRigth
+			else //appearOnRight
 				toast+='right: 0; ';
 				
 			toast+='z-index: 999999; position: fixed; top: '+$.toaster.currentTop+'px; margin:20px" id="toast-'+$.toaster.currentId+'" data-alert class="alert-box '+type+'">'+message+' &nbsp;&nbsp;&nbsp;<a href="#" onclick="$.toaster.hide('+$.toaster.currentId+')" class="close">&times;</a></div>';
 			
 		}
 		$("body").append(toast); //inserts the toast on the page.
-		$.toaster.currentTop+=$("#toast-"+$.toaster.currentId).outerHeight()+10; //increase the top prop to the apropriate value for the next toast appear (10 is the margin)
+		$.toaster.currentTop+=$("#toast-"+$.toaster.currentId).outerHeight()+10; //increase the top prop to the appropriate value for the next toast appear (10 is the margin)
 		$("#toast-"+$.toaster.currentId).hide().slideDown("fast"); //Immediately hide the toast and show it again. This way the user see an awesome transition :)
 		setTimeout(function(){ //Define the toasts timeout
 			$.toaster.hide($.toaster.currentId);
@@ -121,12 +121,12 @@ $.toaster = {
 			$("#toast-"+toast).slideUp("fast",function(){$(this).remove()}); //only hide the toast with an animation and so remove it from the html
 		}else{ //desktop
 			$("#toast-"+toast).slideUp("fast",function(){ //hide the toast with an animation and...
-				var thisHeigth=$("#toast-"+toast).outerHeight()+10; //grab the heigth of the toast to remove (10 is the margin)...
-				$.toaster.currentTop-=thisHeigth; //Update currentTop to be the correct position for the next toast.
+				var thisHeight=$("#toast-"+toast).outerHeight()+10; //grab the height of the toast to remove (10 is the margin)...
+				$.toaster.currentTop-=thisHeight; //Update currentTop to be the correct position for the next toast.
 				$(this).remove(); //properly remove the toast from the html
 				for(i=id;i<$.toaster.activeToasts.length;i++){ //for each remaining toast, rearrange they on the screen (avoid wrong spaces between two toasts)
 					$( "#toast-"+$.toaster.activeToasts[i] ).animate({ //move they up with an animation
-						top: parseInt($("#toast-"+$.toaster.activeToasts[i] ).css("top"))-thisHeigth
+						top: parseInt($("#toast-"+$.toaster.activeToasts[i] ).css("top"))-thisHeight
 					}, 100);
 				}
 			});
